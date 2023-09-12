7367454b724f61d14d31ca765162d08e7fe26e81
CodeSandbox <support@codesandbox.io>
2023-09-12 01:01:27 +0000

    Initial commit

---
+++ components/Logo.tsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+    return (
+        <>
+            <svg className="flex items-center justify-center" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M148.632 332.053L86 394.684L117.316 426L184.421 363.368L229.158 372.316L202.316 345.474L254.095 285.41L394.684 426L426 394.684L283.091 251.775L426 86L261.746 230.43L139.684 108.368L86 86L108.368 139.684L228.419 259.735L166.526 314.158L139.684 287.316L148.632 332.053Z" fill="white"/>
+            </svg>
+        </>
+    )
+}
