881f5ea2c6f0913d7363f952842de222a053b812
real2two <26689496+real2two@users.noreply.github.com>
2023-09-16 14:10:49 -0400

    Revert "Doing more stuff so it works on the bot"
    This reverts commit 996fc3d3c7cc501107d40c07ad77c8065c15118a.

--- app/page.tsx
+++ app/page.tsx
@@ -22,9 +22,9 @@ export default function Home() {
               Manage Servers
             </div>
           </Link> */}
-          <Link className="ml-auto transition hover:bg-[#ffffff11] text-white" href="%landingLoginLink%">
+          <Link className="ml-auto transition hover:bg-[#ffffff11] text-white" href="/login">
             <div className="w-40 h-16 p-4 flex items-center justify-center text-sm font-bold">
-              %landingLoginText%
+              Login
             </div>
           </Link>
                     <div className="border-r-2 border-[transparent]">
@@ -47,7 +47,7 @@ export default function Home() {
           </div>
           <div className="w-full max-w-5xl mt-8 border-y-2 border-[#ffffff11] border-dashed">
             <div className="max-w-[700px] w-full mx-auto">
-              <a className="w-fit h-fit pl-7 px-8 py-5 flex flex-row items-center gap-4 bg-[#fff] text-black rounded-sm transition hover:bg-[#5865F2] hover:text-white" href="%landingLoginLink%">
+              <a className="w-fit h-fit pl-7 px-8 py-5 flex flex-row items-center gap-4 bg-[#fff] text-black rounded-sm transition hover:bg-[#5865F2] hover:text-white" href="/login">
                 <svg
                   className="w-4 h-4 fill-current"
                   role="img"
@@ -56,7 +56,7 @@ export default function Home() {
                   <title>Discord</title>
                   <path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z"></path>
                 </svg>
-                %landingLoginButtonText%
+                Login with Discord
               </a>
             </div>
           </div>
@@ -107,7 +107,7 @@ export default function Home() {
                 </clipPath>
               </defs>
             </svg>
-            <Image className="z-[2]" quality={100} alt="" fill src="/assets/img/landingHero2.png"></Image>
+            <Image className="z-[2]" quality={100} alt="" fill src="/hero2.png"></Image>
           </div>
           <div className="max-w-6xl w-full absolute pointer-events-none border-b-2 border-[#ffffff11] border-dashed bottom-0"></div>
         </div>
